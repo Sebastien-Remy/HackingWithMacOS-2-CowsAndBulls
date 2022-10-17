8d8153ada8c95eac07ceb5aeb59ad1d2f0f6e8f1
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-17 07:53:02 +0200

    Create UI

--- CowsAndBulls/ContentView.swift
+++ CowsAndBulls/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var guess = ""
+    @State private var guesses = [String]()
+    @State private var answer = ""
+    let answerLength = 4
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            HStack {
+                TextField("Enter a guess…", text: $guess)
+                Button("Go", action: submitGuess)
+            }
+            .padding()
+            List(guesses, id: \.self) { guess in
+                HStack {
+                    Text(guess)
+                    Spacer()
+                    Text(result(for: guess))
+                }
+            }
         }
-        .padding()
+        .frame(width: 250)
+        .frame(minWidth: 300, maxHeight: .infinity)
+        .onAppear(perform: startNewGame)
+    }
+    
+    func startNewGame() {
+        guess = ""
+        guesses.removeAll()
+        
+        let numbers = (0...9).shuffled()
+        
+        for i in 0..<answerLength {
+            answer.append(String(numbers[i]))
+        }
+        
+    }
+    
+    func submitGuess() {
+        guesses.append(guess)
+        guess = ""
+    }
+    
+    func result(for guess: String) -> String {
+        "Result"
     }
 }
 
