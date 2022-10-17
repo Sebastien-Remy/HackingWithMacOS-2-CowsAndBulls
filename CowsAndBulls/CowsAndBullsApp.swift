144a75caa6edcf951de4bddccaf9e9ed5d90857a
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-17 06:41:16 +0200

    Initial Commit

---
+++ CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Sebastien REMY on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
